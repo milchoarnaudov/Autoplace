6a7e2e9a4f8e89f4ca1660ef2bd5d43a3fa38b8a
Milcho <milchoarnaudov9807@abv.bg>
2020-12-12 00:55:55 +0200

    Users Seeder bugfix

--- src/Data/AutoPlace.Data/Seeding/ApplicationDbContextSeeder.cs
+++ src/Data/AutoPlace.Data/Seeding/ApplicationDbContextSeeder.cs
@@ -25,8 +25,8 @@
 
             var seeders = new List<ISeeder>
                           {
-                              new UsersSeeder(),
                               new RolesSeeder(),
+                              new UsersSeeder(),
                               new CarModelsSeeder(),
                               new CarTypesSeeder(),
                               new AutopartCategoriesSeeder(),
--- src/Data/AutoPlace.Data/Seeding/UsersSeeder.cs
+++ src/Data/AutoPlace.Data/Seeding/UsersSeeder.cs
@@ -16,23 +16,30 @@
                 return;
             }
 
+            var adminRole = dbContext.Roles.Where(x => x.Name == "Administrator").FirstOrDefault();
             var admin = new ApplicationUser
             {
-                UserName = "admin",
+                UserName = "admin@admin.com",
                 Email = "admin@admin.com",
+                NormalizedEmail = "admin@admin.com".ToUpper(),
+                NormalizedUserName = "admin@admin.com".ToUpper(),
             };
 
-            var adminPassword = new PasswordHasher<ApplicationUser>().HashPassword(admin, "Test123!");
-            admin.PasswordHash = adminPassword;
+            admin.PasswordHash = new PasswordHasher<ApplicationUser>().HashPassword(admin, "Test123!");
 
+            admin.Roles.Add(new IdentityUserRole<string> { RoleId = adminRole.Id });
+
+            var userRole = dbContext.Roles.Where(x => x.Name == "User").FirstOrDefault();
             var user = new ApplicationUser
             {
-                UserName = "user",
+                UserName = "user@user.com",
                 Email = "user@user.com",
+                NormalizedEmail = "user@user.com".ToUpper(),
+                NormalizedUserName = "user@user.com".ToUpper(),
             };
+            user.PasswordHash = new PasswordHasher<ApplicationUser>().HashPassword(user, "Test123!");
 
-            var userPassword = new PasswordHasher<ApplicationUser>().HashPassword(user, "Test123!");
-            user.PasswordHash = userPassword;
+            user.Roles.Add(new IdentityUserRole<string> { RoleId = userRole.Id });
 
             await dbContext.ApplicationUsers.AddAsync(admin);
             await dbContext.ApplicationUsers.AddAsync(user);
