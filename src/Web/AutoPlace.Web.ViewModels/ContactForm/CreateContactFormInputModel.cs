6257bfb0415852e9dc7c6bea8fbc97d36ac6543c
Milcho <milchoarnaudov9807@abv.bg>
2020-12-13 13:10:31 +0200

    Add basic validations for Contact Form

--- src/Web/AutoPlace.Web.ViewModels/ContactForm/CreateContactFormInputModel.cs
+++ src/Web/AutoPlace.Web.ViewModels/ContactForm/CreateContactFormInputModel.cs
@@ -1,13 +1,25 @@
 ﻿namespace AutoPlace.Web.ViewModels.ContactForm
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class CreateContactFormInputModel
     {
+        [Display(Name = "Full Name")]
+        [StringLength(maximumLength: 255, MinimumLength = 5)]
+        [Required]
         public string FullName { get; set; }
 
+        [StringLength(maximumLength: 255, MinimumLength = 5)]
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
+        [StringLength(maximumLength: 100, MinimumLength = 5)]
+        [Required]
         public string Topic { get; set; }
 
+        [StringLength(maximumLength: 500, MinimumLength = 10)]
+        [Required]
         public string Message { get; set; }
     }
 }
