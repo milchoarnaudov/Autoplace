9870b1e242075dd41705c014237bdce4fc498194
Milcho <milchoarnaudov9807@abv.bg>
2020-12-20 21:38:06 +0200

    Scaffold and change Login and Register Pages

---
+++ src/Web/AutoPlace.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AutoPlace.Data;
+using AutoPlace.Data.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AutoPlace.Web.Areas.Identity.IdentityHostingStartup))]
+namespace AutoPlace.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
